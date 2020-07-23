b3f8426933283063ddbc50876328db89eff55dcd
Ivan Mercedes <ivanmercede@gmail.com>
2020-07-23 15:50:57 +0200

    Adding AuthController to User

---
+++ app/controllers/admin/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Models\User;
+use Sirius\Validation\Validator;
+use App\Controllers\BaseController;
+
+class UserController extends BaseController{
+
+    public function getIndex(){
+        $users = User::all();
+        return $this->view('admin/users.twig', [
+            'users' => $users
+        ]);
+    }
+
+    public function getCreate(){
+        return $this->view('admin/insert-user.twig');
+    }
+
+    public function postCreate(){
+        $errors = [];
+        $result = false;
+        $validator = new Validator();
+        $validator->add('name', 'required');
+        $validator->add('email', 'required');
+        $validator->add('email', 'email');
+        $validator->add('password', 'required');
+
+        if($validator->validate($_POST)){
+            $user = new User();
+            $user->name = $_POST['name'];
+            $user->email = $_POST['email'];
+            $user->password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+
+            $user->save();
+
+            $result = true;
+        }else{
+            $errors = $validator->getMessages();
+        }
+
+        return $this->view('admin/insert-user.twig',['result'=>$result,'errors'=>$errors]);
+    }
+    
+}
